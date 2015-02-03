f2595c7691b4de3afaa6dd8b43cbeea2280dd24e
Stéphane MBAPE <rose.martin-8og3raxw@yopmail.com>
2015-02-03 22:31:44 +0000

    sm-array-stream
    git-svn-id: http://127.0.0.1:20080/svn/buma/branches/tutorial/library/sm-array-stream@327 b2333513-9f29-43eb-bce0-aa7d4792c706

---
+++ sm-array-stream.js
@@ -0,0 +1,109 @@
+var util = require('util');
+var Duplex = require('readable-stream').Duplex;
+
+// http://codewinds.com/blog/2013-08-31-nodejs-duplex-streams.html
+
+var ArrayStream = module.exports = function ArrayStream(arr, options) {
+    var self = this;
+    this._reading = false;
+    this._closing = false;
+    this._index = 0;
+    options = options || {};
+    this._duplex = options.duplex || false;
+
+    this.set(arr);
+
+    Duplex.call(this, {
+        objectMode: true,
+        highWaterMark: options.highWaterMark || Math.pow(2, 10)
+    });
+}
+
+util.inherits(ArrayStream, Duplex);
+
+ArrayStream.prototype.end = function() {
+    Duplex.prototype.end.apply(this, arguments);
+    this.close();
+};
+
+ArrayStream.prototype._write = function(chunk, encoding, cb) {
+    if (this._duplex) {
+        this.performWrite(chunk, encoding, cb);
+    }
+    return true;
+};
+
+ArrayStream.prototype.performWrite = function(chunk, encoding, cb) {
+    if (Array.isArray(chunk)) {
+        if (this._buffer) {
+            this._buffer.push.apply(this._buffer, chunk);
+        } else {
+            this._buffer = Array.prototype.slice.call(chunk);
+        }
+    }
+    if (this._inWaiting) {
+        this.push(this._buffer[this._index++]); // will trigger read
+    }
+    if ('function' === typeof cb) {
+        cb();
+    }
+};
+
+ArrayStream.prototype.set = function(chunk) {
+    if (this._closing) return false;
+    if (!this._buffer) {
+        this.performWrite(chunk);
+    }
+};
+
+ArrayStream.prototype.close = function() {
+    var self = this;
+    this._closing = true;
+
+    // Wait for previous read to end
+    setImmediate(function() {
+        self.push(null); // we are done, push null to end stream
+    });
+}
+
+function noMoreData(stream, n) {
+    if (stream._duplex) {
+        stream._inWaiting = n;
+        stream._reading = false;
+    } else {
+        setImmediate(function() {
+            stream.push(null); // we are done, push null to end stream
+        });
+    }
+}
+
+ArrayStream.prototype._read = function(n) {
+    if (this._reading || this._closing) return false;
+    var self = this;
+
+    if (!this._buffer) {
+        noMoreData(this, n);
+        return;
+    }
+
+    this._reading = true;
+    if (this._index >= this._buffer.length) {
+        noMoreData(this, n);
+        return;
+    }
+
+    this._reading = false;
+    
+    for (var i = 0; this._index < this._buffer.length && i < n; this._index++, i++) {
+        if (!this.push(this._buffer[this._index])) {
+            break; // false from push, stop reading
+        }
+    }
+
+    if (this._duplex) {
+        this._inWaiting = 0;
+        if (i < n) {
+            this._inWaiting += n - i;
+        }
+    }
+};
---
+++ test/sm-array-stream.test.js
@@ -0,0 +1,144 @@
+var ArrayStream = require("../");
+var async = require('async');
+
+module.exports = {
+    testConstructor: testConstructor,
+    testBasicStream: testBasicStream,
+    testSet: testSet,
+    testControlFlow: testControlFlow,
+    testDuplex: testDuplex
+};
+
+function testConstructor(assert) {
+    assert.ok(new ArrayStream());
+    assert.ok(new ArrayStream([]));
+    assert.ok(new ArrayStream([1, 2, 3]));
+    assert.ok(new ArrayStream([1, 2, 3], {
+        highWaterMark: 10
+    }));
+    assert.done();
+}
+
+function testBasicStream(assert) {
+    var arr = [1, 2, 3],
+        stream = new ArrayStream(arr),
+        index = 0;
+
+    stream.on('data', function(chunk) {
+        assert.strictEqual(chunk, arr[index++]);
+    });
+
+    stream.once('end', function() {
+        assert.strictEqual(index, 3);
+        assert.done();
+    });
+}
+
+function testSet(assert) {
+    var arr = [1, 2, 3],
+        stream = new ArrayStream(),
+        index = 0;
+
+    stream.pause();
+
+    stream.on('data', function(chunk) {
+        assert.strictEqual(chunk, arr[index++]);
+    });
+
+    stream.once('end', function() {
+        assert.strictEqual(index, 3);
+        assert.done();
+    });
+
+    stream.set(arr);
+    stream.resume();
+}
+
+function testControlFlow(assert) {
+    var arr = [1, 2, 3],
+        stream = new ArrayStream(arr),
+        index = 0;
+
+    stream.pause();
+
+    stream.on('data', function(chunk) {
+        assert.strictEqual(chunk, arr[index++]);
+    });
+
+    stream.once('end', function() {
+        assert.done();
+    });
+
+    setTimeout(function() {
+        assert.strictEqual(index, 0);
+        stream.once('data', function(chunk) {
+            assert.strictEqual(chunk, arr[index - 1]);
+            stream.pause();
+            setTimeout(function() {
+                assert.strictEqual(index, 1);
+                stream.once('data', function(chunk) {
+                    assert.strictEqual(chunk, arr[index - 1]);
+                    stream.pause();
+                    setTimeout(function() {
+                        assert.strictEqual(index, 2);
+                        stream.resume();
+                        stream.close();
+                        assert.strictEqual(index, 2);
+                    }, 5);
+                });
+                stream.resume();
+            }, 5);
+        });
+        stream.resume();
+    }, 5);
+}
+
+function testDuplex(assert) {
+    var arr = [1, 2, 3],
+        add = [4, 5, 6],
+        stream = new ArrayStream(arr, {
+            duplex: true
+        }),
+        index = 0;
+
+    stream.on('data', function(chunk) {
+        assert.strictEqual(chunk, arr[index++]);
+        if (index === 3) {
+            arr.push.apply(arr, add);
+            stream.write(add, function() {
+                stream.once('data', function(chunk) {
+                    assert.strictEqual(index, 4);
+                });
+                stream.end();
+            });
+        }
+    });
+
+    stream.once('end', function() {
+        assert.done();
+    });
+
+}
+
+if (!/\bnodeunit$/.test(process.argv[1])) {
+    var reporter = require('nodeunit').reporters.default;
+    reporter.run({
+        test: module.exports
+    });
+} else if (false) {
+    // For debugging purpose
+    var assert = require('assert');
+    var testSuite = module.exports;
+    tests = [];
+    for (prop in testSuite) {
+        (function(fn) {
+            tests.push(function(next) {
+                assert.done = next;
+                fn(assert);
+            });
+        })(testSuite[prop]);
+    }
+    async.series(tests, function() {
+        console.log('done');
+    });
+}
